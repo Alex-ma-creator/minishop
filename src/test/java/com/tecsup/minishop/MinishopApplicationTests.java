5b7ca454807ca7dfcecc5c20860b14161c9a664a
Alex Huayta <alexhuayta2007@gmail.com>
2026-06-15 22:49:12 -0500

    fix: corregir test deliberado

--- src/test/java/com/tecsup/minishop/MinishopApplicationTests.java
+++ src/test/java/com/tecsup/minishop/MinishopApplicationTests.java
@@ -1,15 +1,12 @@
 package com.tecsup.minishop;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MinishopApplicationTests {
 
-	@Test
-void testFallaDeliberada() {
-    assertEquals(99999, 1);
-}
+    @Test
+    void contextLoads() {
+    }
 }
